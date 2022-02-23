f170a38cb40965360b4b884c9dc2957aeb899629
cmsjulio <cms.julio1@gmail.com>
2022-02-23 14:22:24 -0300

    Rota configurada (redirecionando '/' para '/escolas'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EscolaListComponent } from './escola-list/escola-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'escolas', component: EscolaListComponent},
+  {path: '', redirectTo: 'escolas', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
